cc033f388ad3b5af6cf1252dca4569c4e6924197
Samolxys <samolxys.gmail.com>
2021-05-19 14:21:21 +0300

    + Sprites Grid & PlayerLogic
    Sprites for the environment and characters one for each – no animation
    Created and placed floor and walls grid.
    Added cross platform logic for the player char

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] int playerLife = 20;
+    [SerializeField] float runSpeed = 5f;
+
+
+    Rigidbody2D myRigidBody;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlipSprite();
+             RunV();
+       RunH();
+
+    }
+
+    private void RunH()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.x);
+        myRigidBody.velocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+    }
+
+    private void RunV()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+       bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+}
